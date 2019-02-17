a0e58255b77bb975ecd68d5f691d16987f13269c
Jonathan Ary <aryjonathan@icloud.com>
2019-02-17 18:32:54 +0200

    deleted some comments

--- HideAndSeek/Timer.swift
+++ HideAndSeek/Timer.swift
@@ -6,58 +6,7 @@
 //  Copyright © 2019. Jonathan. All rights reserved.
 //
 
-//import Foundation
 
-//class Counter {
-//
-//    var timer: Timer
-//    var runCount = 0
-//    init() {
-//        timer = Timer.init(timeInterval: 1.0, target: Counter.self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
-//    }
-//    @objc func fireTimer(_: Timer) {
-//        print("Timer fired")
-//        runCount += 1
-//        if runCount == 60 {
-//            timer.invalidate()
-//        }
-//    }
-//}
-
-
-//class MyTimer: NSObject {
-//
-//    var startTime = TimeInterval()
-//    var time: Double = 4
-//    var timer = Timer()
-//    var timerEndedCallback: (() -> Void)!
-//    var timerInProgressCallback: ((_ elapsedTime: Double) -> Void)!
-//
-//    func startTimer(timerEnded: @escaping () -> Void, timerInProgress: ((_ elapsedTime: Double) -> Void)!) {
-//        if !timer.isValid {
-//            let aSelector = #selector(MyTimer.updateTime)
-//            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: aSelector, userInfo: nil, repeats: true)
-//            startTime = 60.0
-//            timerEndedCallback = timerEnded
-//            timerInProgressCallback = timerInProgress
-//        }
-//    }
-//
-//    @objc func updateTime() {
-//
-//        var currentTime = NSDate.timeIntervalSinceReferenceDate
-//        var elapsedTime = currentTime - startTime
-//        var seconds = time - elapsedTime
-//
-//        if seconds > 0 {
-//            elapsedTime -= TimeInterval(seconds)
-//            timerInProgressCallback(elapsedTime)
-//        } else {
-//            timer.invalidate()
-//            timerEndedCallback()
-//        }
-//    }
-//}
 
 
 import UIKit
